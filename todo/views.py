34fb0ac98bd888f1f070c228e3ec1ad893161cf9
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-04-02 11:04:38 +0100

    eleventh commit

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 def home(request):
@@ -59,4 +60,5 @@ def createtodo(request):
             return render(request, "todo/createtodo.html", {"form": TodoForm(), "error":"Bad data passed in"})
 
 def currenttodos(request):
-    return render(request, "todo/currenttodos.html")
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "todo/currenttodos.html",{"todos":todos})
